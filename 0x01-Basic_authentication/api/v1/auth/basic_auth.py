8166b4bdb3de860b4a6f3f90bb99c69cdec367fb
yasssertawfek1990 <dr.yassertawfek@gmail.com>
2024-07-09 18:56:56 +0300

    c

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-"""handle basic auth"""
+"""dc ms djjkjkj j kjnsd jsdkjn ds kjnds kkljlds"""
 import base64
 from typing import Tuple, TypeVar
 from api.v1.auth.auth import Auth
@@ -7,24 +7,13 @@ from models.user import User
 
 
 class BasicAuth(Auth):
-    """- defines the attributes and methods for basic
-    authorization
-    - inherits from Auth
-    Args:
-        Auth (class): Parent authentication class
-    """
+    """hjb as cnkjlas ckjjnkjn aslcn nl nlknjaksnc kjn kjnlknas csac"""
 
     def extract_base64_authorization_header(
             self,
             authorization_header: str
             ) -> str:
-        """returns the Base64 part of the Authorization header
-        for a Basic Authentication
-        Args:
-            authorization_header (str): auth_header
-        Returns:
-            str: base64 part of header
-        """
+        """sdbj kksd khjbsdb kjbsdjkb jkhb sdkl sd nljksdkl klsdkj sd"""
         if not (authorization_header and isinstance(authorization_header, str)
                 and authorization_header.startswith('Basic ')):
             return None
@@ -35,12 +24,7 @@ class BasicAuth(Auth):
             self,
             base64_authorization_header: str
             ) -> str:
-        """returns the decoded value of a Base64 string
-        Args:
-            base64_authorization_header (str): base64 auth header
-        Returns:
-            str: decoded value of base64 string
-        """
+        """rd kjdskv jb sdjkhbv hkhsdkjbkj dskjb jhbks dckh kjbsdb cjkkjdsh sdc"""
         if not (base64_authorization_header and
                 isinstance(base64_authorization_header, str)):
             return None
@@ -54,12 +38,7 @@ class BasicAuth(Auth):
             self,
             decoded_base64_authorization_header: str
             ) -> Tuple[str, str]:
-        """extracts user email and password
-        from the Base64 decoded value.
-        Args:
-            self (obj): Basic Auth instance
-            decoded_base64_authorization_header (str): auth header
-        """
+        """esdk ks lsjdcl jkls dbblsdbv lknlksdvjk kjnjks nkjn sd"""
         if not (decoded_base64_authorization_header and
                 isinstance(decoded_base64_authorization_header, str) and
                 ':' in decoded_base64_authorization_header):
@@ -69,12 +48,7 @@ class BasicAuth(Auth):
 
     def user_object_from_credentials(
             self, user_email: str, user_pwd: str) -> TypeVar('User'):
-        """returns the User instance based on his email and password.
-        Args:
-            self (_type_): Basic auth instance
-            user_email(str): user email
-            user_pwd(str): user pwd
-        """
+        """rekusd uo usdu ciuninu sldi niniouds inion i nids oiun oin psd sddd"""
         if not (user_email and isinstance(user_email, str) and
                 user_pwd and isinstance(user_pwd, str)):
             return None
@@ -91,7 +65,7 @@ class BasicAuth(Auth):
         return None
 
     def current_user(self, request=None) -> TypeVar('User'):
-        """get current user"""
+        """gl sld csd lnkljdsjcn ln kljn dskj nlkjn kljsdn kljdsn dsc"""
         try:
             auth_header = self.authorization_header(request)
             encoded = self.extract_base64_authorization_header(auth_header)
